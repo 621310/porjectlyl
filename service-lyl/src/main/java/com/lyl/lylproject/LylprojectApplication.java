c6de1fc9dbe59bc30ef4fb5578c5c61dfc1fc4b3
liuyiling <2099602858@qq.com>
2020-04-19 13:53:37 +0800

    提交

---
+++ service-lyl/src/main/java/com/lyl/lylproject/LylprojectApplication.java
@@ -0,0 +1,13 @@
+package com.lyl.lylproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LylprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LylprojectApplication.class, args);
+    }
+
+}
---
+++ service-lyl/src/test/java/com/lyl/lylproject/LylprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyl.lylproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LylprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
